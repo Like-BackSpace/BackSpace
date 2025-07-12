fbbe70b444abfca03e0cd74ab002a2fc3f664146
Like-BackSpace <mitanuragpal@gmail.com>
2025-07-13 01:26:23 +0530

    Improve some styling

--- src/components/Feature.jsx
+++ src/components/Feature.jsx
@@ -15,13 +15,13 @@ const Feature = () => {
             <div className='flex flex-wrap mt-10 lg:mt-20 ml-30 mr-30'>
                 {features.map((feature, index) => (
                     <div key={index} className='w-full sm:1/2 lg:w-1/3 '>
-                        <div className='flex mr-3 ml-3 mb-10 border-1 rounded-lg border-neutral-700 space-x-4'>
+                        <div className='flex mr-3 ml-3 mb-10  border-1 rounded-lg border-neutral-700 space-x-4'>
                             <div
-                                className='flex mx-6 h-10 w-10  bg-neutral-900 text-lime-500 justify-center items-center rounded-full'>
+                                className='flex mx-6 h-10 w-10 mt-3 bg-neutral-900 text-lime-500 justify-center items-center rounded-full'>
                                 {feature.icon}
                             </div>
                             <div>
-                                <h5 className='mt-1 mb-6 text-xl'>{feature.text}</h5>
+                                <h5 className='mt-3  mb-6 text-xl'>{feature.text}</h5>
                                 <p className='text-md  mb-20 text-neutral-500 '>{feature.description}</p>
                             </div>
                         </div>
